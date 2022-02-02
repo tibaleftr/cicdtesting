e25eff68fc796fbeeff039b441e563ace74215bb
tibaleftr <thomas.bales@ftr.com>
2022-02-02 10:04:50 -0600

    Initial commit

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class CICD(unittest.TestCase):
     def setUp(self):
         self.assertTrue(True)
 
+    def test_cicd(self):
+        self.assertTrue(True)
+
 
 if __name__ == '__main__':
     unittest.main()
